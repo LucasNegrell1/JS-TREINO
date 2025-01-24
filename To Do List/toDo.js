41c36eb0e1fc21281094e81ff267b246e73326de
LucasNegrell1 <lucasmcnegrelli@gmail.com>
2025-01-24 15:39:37 -0300

    make a delete buton and choose the date of the task

--- To Do List/toDo.js
+++ To Do List/toDo.js
@@ -1,11 +1,26 @@
-const todoList = []
+const todoList = [{
+   name: 'make dinner',
+   dueDate: '2025-01-29'
+}, {
+   name: 'wash dishes',
+   dueDate: '2025-01-29'
+}
+]
 
 function renderTodoList (){
    let todoListHTML = ''
 
    for(let i = 0; i < todoList.length; i++) {
-      const todo = todoList[i]
-      const html = ` <p>${todo}</p> `
+      const todoObject = todoList[i]
+      const { name, dueDate } = todoObject
+         const html = ` <p>
+         ${name} ${dueDate} 
+         <button onclick=" 
+         todoList.splice(${i}, 1); 
+      renderTodoList();
+         ">Delete</button>   
+         </p>
+      `
       todoListHTML += html
 
    }
