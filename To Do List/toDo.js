c85998d34e5408ef578dde9beb50d8ef1bfa8b65
LucasNegrell1 <lucasmcnegrelli@gmail.com>
2025-01-25 14:37:21 -0300

    making the Grid and Styling those buttons

--- To Do List/toDo.js
+++ To Do List/toDo.js
@@ -13,13 +13,13 @@ function renderTodoList (){
    for(let i = 0; i < todoList.length; i++) {
       const todoObject = todoList[i]
       const { name, dueDate } = todoObject
-         const html = ` <p>
-         ${name} ${dueDate} 
-         <button onclick=" 
-         todoList.splice(${i}, 1); 
+         const html = ` 
+        <div> ${name} </div>
+         <div> ${dueDate} </div>
+      <button onclick=" 
+      todoList.splice(${i}, 1); 
       renderTodoList();
-         ">Delete</button>   
-         </p>
+      " class="delete-todo-button">Delete</button>     
       `
       todoListHTML += html
 
@@ -33,7 +33,13 @@ function addTodo() {
    const inputElement = document.querySelector('.js-name-input');
    const name = inputElement.value;
   
-   todoList.push(name);
+   const dateInputElement = document.querySelector('.js-due-date-input');  
+   const dueDate = dateInputElement.value
+
+   todoList.push({
+      name,
+      dueDate
+   });
    console.log(todoList);
 
    inputElement.value = '';
